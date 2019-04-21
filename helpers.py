74d3cf651a0609574b4b4090811d24a942478875
loganballard <33067676+loganballard@users.noreply.github.com>
2019-04-20 17:31:35 -0700

    making more simple again, relying on documentation

--- helpers.py
+++ helpers.py
@@ -16,7 +16,7 @@ def transaction(db_call):
     def transaction_wrapper(conn, *args, **kwargs):
         try:
             cursor = conn.cursor()
-            db_call(cursor=cursor, *args, **kwargs)
+            db_call(cursor, *args, **kwargs)
             cursor.close()
             conn.commit()
         except pgre.DatabaseError as error:
@@ -25,15 +25,79 @@ def transaction(db_call):
     return transaction_wrapper
 
 @transaction
-def create_schema(conn, schema, cursor=None):
+def create_schema(cursor, schema):
     """
-    Make sure to pass in a connection object! Must pass in arguments by keyword.
-    Creates the named schema. Transaction decorator will pass in a 
-    cursor object.
+    Make sure to pass in a connection object when calling! 
+    Decorator will pass in cursor.
+    Creates the named schema.
     
     Arguments:
-        - conn - a postgres connection object
+        - cursor (conn) - a postgres connection object
         - schema - name of the schema to be created
     """
     cursor.execute(f"CREATE SCHEMA IF NOT EXISTS {schema};")
 
+@transaction
+def create_tables(cursor, schema):
+    """
+    Make sure to pass in a connection object when calling!
+    Creates the tables that the wisconsin benchmark tests: 1k, 10k, 10k2
+    
+    Arguments:
+        - cursor (conn) - a postgres connection object
+        - schema - name of the schema
+    """
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS {schema}.ONEKTUP (
+        unique1         integer NOT NULL,
+        unique2         integer NOT NULL PRIMARY KEY,
+        two             integer NOT NULL,
+        four            integer NOT NULL,
+        ten             integer NOT NULL,
+        twenty          integer NOT NULL,
+        onePercent      integer NOT NULL,
+        tenPercent      integer NOT NULL,
+        twentyPercent   integer NOT NULL,
+        fiftyPercent    integer NOT NULL,
+        unique3         integer NOT NULL,
+        evenOnePercent  integer NOT NULL,
+        oddOnePercent   integer NOT NULL,
+        stringu1        char(52) NOT NULL,
+        stringu2        char(52) NOT NULL,
+        string4         char(52) NOT NULL
+    );""")
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS {schema}.TENKTUP1 (
+        unique1         integer NOT NULL,
+        unique2         integer NOT NULL PRIMARY KEY,
+        two             integer NOT NULL,
+        four            integer NOT NULL,
+        ten             integer NOT NULL,
+        twenty          integer NOT NULL,
+        onePercent      integer NOT NULL,
+        tenPercent      integer NOT NULL,
+        twentyPercent   integer NOT NULL,
+        fiftyPercent    integer NOT NULL,
+        unique3         integer NOT NULL,
+        evenOnePercent  integer NOT NULL,
+        oddOnePercent   integer NOT NULL,
+        stringu1        char(52) NOT NULL,
+        stringu2        char(52) NOT NULL,
+        string4         char(52) NOT NULL
+    );""")
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS {schema}.TENKTUP2 (
+        unique1         integer NOT NULL,
+        unique2         integer NOT NULL PRIMARY KEY,
+        two             integer NOT NULL,
+        four            integer NOT NULL,
+        ten             integer NOT NULL,
+        twenty          integer NOT NULL,
+        onePercent      integer NOT NULL,
+        tenPercent      integer NOT NULL,
+        twentyPercent   integer NOT NULL,
+        fiftyPercent    integer NOT NULL,
+        unique3         integer NOT NULL,
+        evenOnePercent  integer NOT NULL,
+        oddOnePercent   integer NOT NULL,
+        stringu1        char(52) NOT NULL,
+        stringu2        char(52) NOT NULL,
+        string4         char(52) NOT NULL
+    );""")
