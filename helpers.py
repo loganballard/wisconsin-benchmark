f93f06fad2acab520db95c1e6fbab656218fbb9c
loganballard <33067676+loganballard@users.noreply.github.com>
2019-04-22 08:37:12 -0700

    done with data generation, now just have to write automated setup script and then run it on the linuxlab comp to add to main db

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import psycopg2 as pgre
 from os import environ as env
+from random import sample
 
 def get_setup_vars():
     return {
@@ -101,3 +102,67 @@ def create_tables(cursor, schema):
         stringu2        char(52) NOT NULL,
         string4         char(52) NOT NULL
     );""")
+
+@transaction
+def insert_tuple(cursor, vals, schema, table):
+    """
+    Make sure to pass in a connection object when calling!
+    Creates the tables that the wisconsin benchmark tests: 1k, 10k, 10k2
+    
+    Arguments:
+        - cursor (conn) - a postgres connection object
+        - vals - the tuple to insert
+        - schema - name of the schema
+        - table - the table to insert the tuple into
+    """
+    cursor.execute(f"""
+        INSERT INTO {schema}.{table} (unique1, unique2, two, four, ten, twenty, onePercent, tenPercent, twentyPercent, fiftyPercent, unique3, evenOnePercent, oddOnePercent, stringu1, stringu2, string4)
+        VALUES ({vals[0]}, {vals[1]}, {vals[2]}, {vals[3]}, {vals[4]}, {vals[5]}, {vals[6]}, {vals[7]}, {vals[8]}, {vals[9]}, {vals[10]}, {vals[11]}, {vals[12]}, '{vals[13]}', '{vals[14]}', '{vals[15]}');
+    """)
+
+def gen_stringu1_2(uniq):
+    ret = ['A','A','A','A','A','A','A']
+    i = 6
+    while uniq > 0:
+        rem = uniq % 26
+        ret[i] = chr(ord('A') + rem)
+        uniq = uniq // 26
+        i -= 1
+    return "".join(ret) + 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+def gen_string4(x):
+    string4 = [
+        'AAAAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'HHHHxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'OOOOxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+        'VVVVxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    ]
+    return string4[x % 4]
+
+def generate_data(conn, schema):
+    """
+    Generates fake data to be inserted into the 1k, tenk1, tenk2,
+    tables.
+
+    Arguments:
+        - cursor (conn) - a postgres connection object
+        - schema - name of the schema
+    """
+    ten_thous = sample(range(10000), 10000)
+    for uniq2 in range(0, 10000):
+        uniq1 = uniq3 = ten_thous[uniq2]
+        two = fiftyP = uniq1 % 2
+        four = uniq1 % 4
+        ten = tenP = uniq1 % 10
+        twenty = uniq1 % 20
+        oneP = uniq1 % 100
+        twentyP = uniq1 % 5
+        evenOneP = oneP*2
+        oddOneP = evenOneP + 1
+        stringu1 = gen_stringu1_2(uniq1)
+        stringu2 = gen_stringu1_2(uniq2)
+        string4 = gen_string4(uniq2)
+        vals = (uniq1, uniq2, two, four, ten, twenty, oneP, tenP, twentyP, fiftyP, uniq3, evenOneP, oddOneP, stringu1, stringu2, string4)
+        tables = ['ONEKTUP', 'TENKTUP1', 'TENKTUP2'] if uniq2 < 1000 else ['TENKTUP1', 'TENKTUP2']
+        for table in tables:
+            insert_tuple(conn, vals=vals, schema=schema, table=table)
