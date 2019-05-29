f071f496b8bf45e2eb7549c471c1ebc72ca6ba08
loganballard <33067676+loganballard@users.noreply.github.com>
2019-05-28 22:34:46 -0700

    initial commit for the queries that will be run and benchmarked

---
+++ benchmark-queries.sql
@@ -0,0 +1,21 @@
+-- initial queries for measuring the benchmarks devised in part 2
+
+-- Query 1
+/*
+    the 10% rule of thumb
+*/
+
+-- Query 2
+/*
+    join algorithms - hash vs sort/merge
+*/
+
+-- Query 3
+/*
+    aggregation
+*/
+
+-- Query 4
+/*
+    GEQO Query Optimization
+*/
