7fc69dd70b892456a70d1e692cba08a0eaec4d11
loganballard <33067676+loganballard@users.noreply.github.com>
2019-06-01 00:14:54 -0700

    adding sql and generation code updates for pt 3

--- benchmark-queries.sql
+++ benchmark-queries.sql
@@ -1,19 +1,94 @@
 -- initial queries for measuring the benchmarks devised in part 2
 
--- Query 1
+-- Experiment 1
 /*
     the 10% rule of thumb
+
+    Create a 100k tuple - Done
+*/
+-- Query 2 and 4
+SELECT * 
+FROM HUNDREDKTUP1
+WHERE unique2 BETWEEN 792 AND 1791;
+
+-- Query 6
+SELECT * 
+FROM HUNDREDKTUP1;
+
+-- Index
+CREATE INDEX wisc.idx_hund_uniq2
+ON wisc.HUNDREDKTUP1(unique2);
+
+
+/*
+----------------------------------------------------
 */
 
--- Query 2
+
+-- Experiment 2
 /*
     join algorithms - hash vs sort/merge
 */
 
--- Query 3
+-- Create BPrime
+CREATE TABLE IF NOT EXISTS wisc.BPRIME (
+    unique1         integer NOT NULL,
+    unique2         integer NOT NULL PRIMARY KEY,
+    two             integer NOT NULL,
+    four            integer NOT NULL,
+    ten             integer NOT NULL,
+    twenty          integer NOT NULL,
+    onePercent      integer NOT NULL,
+    tenPercent      integer NOT NULL,
+    twentyPercent   integer NOT NULL,
+    fiftyPercent    integer NOT NULL,
+    unique3         integer NOT NULL,
+    evenOnePercent  integer NOT NULL,
+    oddOnePercent   integer NOT NULL,
+    stringu1        char(52) NOT NULL,
+    stringu2        char(52) NOT NULL,
+    string4         char(52) NOT NULL
+);
+
+-- Populate BPrime
+INSERT INTO BPRIME 
+SELECT * 
+FROM TENKTUP2 
+WHERE TENKTUP2.unique2 < 1000;
+
+-- Query 10 and 13
+SELECT * 
+FROM wisc.TENKTUP1, wisc.BPRIME 
+WHERE (wisc.TENKTUP1.unique2 = wisc.BPRIME.unique2);
+
+-- Query 15
+SELECT * 
+FROM wisc.TENKTUP1, wisc.TENKTUP2 
+WHERE (wisc.TENKTUP1.unique1 = wisc.TENKTUP2.unique1) 
+AND (wisc.TENKTUP1.unique2 < 1000)
+
+-- Query 17
+SELECT * 
+FROM wisc.ONEKTUP, wisc.TENKTUP1, wisc.TENKTUP2
+WHERE (wisc.ONEKTUP.unique1 = wisc.TENKTUP1.unique1) 
+AND (wisc.TENKTUP1.unique1 = wisc.TENKTUP2.unique1) 
+AND (wisc.TENKTUP1.unique1 < 1000)
+
+
+/*
+----------------------------------------------------
+*/
+
+
+-- Experiment 3
 /*
     aggregation
 */
+
+-- Query 22 and 25
+SELECT SUM (wisc.TENKTUP1.unique3) 
+FROM wisc.TENKTUP1 
+GROUP BY wisc.TENKTUP1.onePercent
 
 -- Query 4
 /*
--- helpers.py
+++ helpers.py
@@ -110,6 +110,24 @@ def create_tables(cursor, schema):
         stringu2        char(52) NOT NULL,
         string4         char(52) NOT NULL
     );""")
+    cursor.execute(f"""CREATE TABLE IF NOT EXISTS {schema}.HUNDREDKTUP1 (
+        unique1         integer NOT NULL,
+        unique2         integer NOT NULL PRIMARY KEY,
+        two             integer NOT NULL,
+        four            integer NOT NULL,
+        ten             integer NOT NULL,
+        twenty          integer NOT NULL,
+        onePercent      integer NOT NULL,
+        tenPercent      integer NOT NULL,
+        twentyPercent   integer NOT NULL,
+        fiftyPercent    integer NOT NULL,
+        unique3         integer NOT NULL,
+        evenOnePercent  integer NOT NULL,
+        oddOnePercent   integer NOT NULL,
+        stringu1        char(52) NOT NULL,
+        stringu2        char(52) NOT NULL,
+        string4         char(52) NOT NULL
+    );""")
 
 @transaction
 def insert_tuple(cursor, vals, schema, table):
@@ -147,6 +165,27 @@ def gen_string4(x):
     ]
     return string4[x % 4]
 
+
+def build_vals_tup(uniq2, table_size):
+    """
+
+    """
+    uniq1 = uniq3 = table_size[uniq2]
+    two = fiftyP = uniq1 % 2
+    four = uniq1 % 4
+    ten = tenP = uniq1 % 10
+    twenty = uniq1 % 20
+    oneP = uniq1 % 100
+    twentyP = uniq1 % 5
+    evenOneP = oneP*2
+    oddOneP = evenOneP + 1
+    stringu1 = gen_stringu1_2(uniq1)
+    stringu2 = gen_stringu1_2(uniq2)
+    string4 = gen_string4(uniq2)
+    return (uniq1, uniq2, two, four, ten, twenty, oneP, tenP, twentyP, fiftyP, uniq3, evenOneP, oddOneP, stringu1, stringu2, string4)
+
+
+
 def generate_data(conn, schema):
     """
     Generates fake data to be inserted into the 1k, tenk1, tenk2,
@@ -156,21 +195,19 @@ def generate_data(conn, schema):
         - cursor (conn) - a postgres connection object
         - schema - name of the schema
     """
+    one_thous = sample(range(1000), 1000)
     ten_thous = sample(range(10000), 10000)
-    for uniq2 in range(0, 10000):
-        uniq1 = uniq3 = ten_thous[uniq2]
-        two = fiftyP = uniq1 % 2
-        four = uniq1 % 4
-        ten = tenP = uniq1 % 10
-        twenty = uniq1 % 20
-        oneP = uniq1 % 100
-        twentyP = uniq1 % 5
-        evenOneP = oneP*2
-        oddOneP = evenOneP + 1
-        stringu1 = gen_stringu1_2(uniq1)
-        stringu2 = gen_stringu1_2(uniq2)
-        string4 = gen_string4(uniq2)
-        vals = (uniq1, uniq2, two, four, ten, twenty, oneP, tenP, twentyP, fiftyP, uniq3, evenOneP, oddOneP, stringu1, stringu2, string4)
-        tables = ['ONEKTUP', 'TENKTUP1', 'TENKTUP2'] if uniq2 < 1000 else ['TENKTUP1', 'TENKTUP2']
-        for table in tables:
-            insert_tuple(conn, vals=vals, schema=schema, table=table)
+    hun_thous = sample(range(100000), 100000)
+    for uniq2 in range(0, 100000):
+        hun_vals = build_vals_tup(uniq2, hun_thous)
+        insert_tuple(conn, vals=hun_vals, schema=schema, table='HUNDREDKTUP1')
+        if uniq2 < 1000:
+            one_vals = build_vals_tup(uniq2, one_thous)
+            ten_vals = build_vals_tup(uniq2, ten_thous)
+            insert_tuple(conn, vals=one_vals, schema=schema, table='ONEKTUP')
+            insert_tuple(conn, vals=ten_vals, schema=schema, table='TENKTUP1')
+            insert_tuple(conn, vals=ten_vals, schema=schema, table='TENKTUP2')
+        elif uniq2 < 10000:
+            ten_vals = build_vals_tup(uniq2, ten_thous)
+            insert_tuple(conn, vals=ten_vals, schema=schema, table='TENKTUP1')
+            insert_tuple(conn, vals=ten_vals, schema=schema, table='TENKTUP2')
